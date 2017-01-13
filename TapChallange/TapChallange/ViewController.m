39b972788f7f4875a6904cdfbaad833fa74a45e7
munsra <piero.silvestri93@gmail.com>
2017-01-13 17:41:26 +0100

    TapChallange Complete
    Completato il gioco "TapChallange"

--- TapChallange/TapChallange/ViewController.m
+++ TapChallange/TapChallange/ViewController.m
@@ -8,8 +8,14 @@
 
 #import "ViewController.h"
 
+#define GameTimer 1 // tempo di stick
+#define GameTime 5 // definisco il tempo di una partita
+
 @interface ViewController (){
     int _tapCounter;
+    int _timeCount;
+    
+    NSTimer *_gameTimer;
 }
 
 @end
@@ -19,12 +25,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tapsCountLabel.minimumScaleFactor = 0.5;
+    [self.tapsCountLabel setAdjustsFontSizeToFitWidth:true];
+    
+    [self initializeGame];
+    
 }
 
+-(void)initializeGame{
+    _tapCounter = 0;
+    _timeCount = GameTime;
+    
+    [self.tapsCountLabel setText:@"Tap to Play"];
+    [self.timeLabel setText:[NSString stringWithFormat:@"Tap Challenge - %i sec", _timeCount]];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+-(IBAction)buttonPressed:(id)sender{
+    NSLog(@"buttonPressed: %i", _tapCounter);
+    
+    if (_gameTimer == nil){
+        _gameTimer = [NSTimer scheduledTimerWithTimeInterval:(GameTimer) target:self selector:@selector(timerTick) userInfo:nil repeats:true];
+    }
+    
+    
+    _tapCounter++;
+    
+    [self.tapsCountLabel setText:[NSString stringWithFormat:@"%i", _tapCounter]];
+}
+
+-(void)timerTick{
+    // Quando finisce il mio timer+
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    _timeCount--;
+    
+    [self.timeLabel setText:[NSString stringWithFormat:@"%i sec", _timeCount]];
+    
+    if(_timeCount == 0){
+        [_gameTimer invalidate];
+        _gameTimer = nil;
+        
+        [self initializeGame];
+    }
+
 }
 
 
