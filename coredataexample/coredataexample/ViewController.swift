c01b7d580fe0977e58e6dd310c9802b474e8a97d
munsra <piero.silvestri93@gmail.com>
2017-06-08 17:18:33 +0200

    Finito Coredata
    Finito tutto

--- coredataexample/coredataexample/ViewController.swift
+++ coredataexample/coredataexample/ViewController.swift
@@ -11,11 +11,11 @@ import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var people: [String] = [];
+    var people: [Person] = [];
 
     @IBOutlet weak var tableViewPeople: UITableView!
     
-    let container = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +31,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        let managedContext = self.container.viewContext
+        let container = self.appDelegate.persistentContainer
+        let managedContext = container.viewContext
         let persons:[Person]
         let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
         // Metodo per recuperare i dati dal database
         do{
             persons = try managedContext.fetch(fetchRequest)
             people = persons.map({ (person: Person) in
-                return person.name!
+                return person
             })
         }catch let error as NSError{
             print("Could not fetch. \(error), \(error.userInfo)")
@@ -58,15 +59,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
             print("OK")
             let textValue = alertController.textFields?.first?.text
-            self.people.append(textValue!)
-            self.tableViewPeople.reloadData()
-            let managedContext = self.container.viewContext
-            let person: Person = NSEntityDescription.insertNewObject(forEntityName: "Person", into: managedContext) as! Person
-            person.name = textValue
-            do{
-                try managedContext.save()
-            }catch let error as NSError{
-                print("Could not fetch. \(error), \(error.userInfo)")
+            let flag = self.save(name: textValue!)
+            if(flag){
+                self.tableViewPeople.reloadData()
             }
         }
         alertController.addAction(cancelAction)
@@ -89,11 +84,93 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = person
+        cell.textLabel?.text = person.name
         return cell
     }
     
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .destructive, title: "Delete"){
+            (action, indexPath) in
+            print("Delete")
+            let container = self.appDelegate.persistentContainer
+            let context = container.viewContext
+            do{
+                context.delete(self.people[indexPath.row])
+                self.people.remove(at: indexPath.row)
+                self.tableViewPeople.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+                try context.save()
+            }catch let error as NSError{
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        }
+        let edit = UITableViewRowAction(style: .normal, title: "Edit"){
+            (action, indexPath) in
+            let alert = UIAlertController(title: "modifica", message: "Modifica elemento", preferredStyle: UIAlertControllerStyle.alert)
+            let defaultAction = UIAlertAction(title: "modifica", style: UIAlertActionStyle.default){
+                (UIAlertAction) in
+                print("Modifica Elemento")
+                let person = self.people[indexPath.row]
+                person.name = alert.textFields?[0].text
+                self.tableViewPeople.reloadData()
+                self.appDelegate.saveContext()
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addTextField{
+                (textField : UITextField) in
+                // textField.delegate = self
+            }
+            alert.addAction(defaultAction)
+            alert.addAction(cancel)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        return  [delete, edit]
+    }
     
+    /*
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    */
+    
+    func save(name: String) -> Bool{
+        let container = self.appDelegate.persistentContainer
+        let context = container.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: context)
+        let person = NSManagedObject(entity: entity!, insertInto: context)
+        
+        person.setValue(name, forKey: "name")
+        do{
+            try context.save()
+            people.append(person as! Person)
+        }catch let error as NSError{
+            print("Could not save. \(error), \(error.userInfo)")
+            return false
+        }
+        
+        return true
+    }
+    
+    func deleteItem(indexPath: IndexPath) -> Bool{
+
+        let container = self.appDelegate.persistentContainer
+        let context = container.viewContext
+        let persons:[Person]
+        let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+        let predicate = NSPredicate(format: "name contains[c] %@", (self.tableViewPeople.cellForRow(at: indexPath)?.textLabel?.text)!)
+        fetchRequest.predicate = predicate
+        // Metodo per recuperare i dati dal database
+        do{
+            persons = try context.fetch(fetchRequest)
+            context.delete(persons.first!)
+            print("Cancellato")
+        }catch let error as NSError{
+            print("Could not fetch. \(error), \(error.userInfo)")
+            return false
+        }
+        
+        return true
+    }
     
 }
 
